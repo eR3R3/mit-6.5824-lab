c3b41dc9a71fd273bdf6adc570c063b464fa4331
eR3R3 <er1r1@qq.com>
2026-01-02 23:46:51 +0800

    feat: lab2 done, all tests passed

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,7 +1,9 @@
 package lock
 
 import (
+	"6.5840/kvsrv1/rpc"
 	"6.5840/kvtest1"
+	"fmt"
 )
 
 type Lock struct {
@@ -9,8 +11,8 @@ type Lock struct {
 	// the specific Clerk type of ck but promises that ck supports
 	// Put and Get.  The tester passes the clerk in when calling
 	// MakeLock().
-	ck kvtest.IKVClerk
-	// You may add code here
+	ck  kvtest.IKVClerk
+	key string
 }
 
 // MakeLock The tester calls MakeLock() and passes in a k/v clerk; your code can
@@ -19,15 +21,83 @@ type Lock struct {
 // Use l as the key to store the "lock state" (you would have to decide
 // precisely what the lock state is).
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
-	lk := &Lock{ck: ck}
-	// You may add code here
+	lk := &Lock{
+		ck:  ck,
+		key: l,
+	}
+
 	return lk
 }
 
-func (lk *Lock) Acquire() {
-	// Your code here
+func (lockWrapper *Lock) Acquire() {
+	success := false
+	for success == false {
+		value, version, err := lockWrapper.ck.Get(lockWrapper.key)
+		valWritten := kvtest.RandValue(8)
+		if err == rpc.ErrNoKey {
+			err = lockWrapper.ck.Put(lockWrapper.key, valWritten, 0)
+			if err != rpc.OK {
+				continue
+			} else {
+				success = true
+			}
+		} else {
+			if value == "" {
+				err = lockWrapper.ck.Put(lockWrapper.key, valWritten, version)
+				if err == rpc.OK {
+					success = true
+				} else {
+					if err == rpc.ErrMaybe {
+						val, _ := lockWrapper.ReadUntilSuccess()
+						if val != valWritten {
+							continue
+						} else {
+							fmt.Println("?")
+							success = true
+						}
+					}
+				}
+			} else {
+				continue
+			}
+		}
+	}
 }
 
-func (lk *Lock) Release() {
-	// Your code here
+func (lockWrapper *Lock) Release() {
+	_, version, _ := lockWrapper.ck.Get(lockWrapper.key)
+	lockWrapper.CheckAndReput(version)
+}
+
+func (lockWrapper *Lock) CheckAndReput(currVersion rpc.Tversion) {
+	success := false
+
+	for success == false {
+		//fmt.Println("stuck at CheckAndReput")
+		_, version := lockWrapper.ReadUntilSuccess()
+		if currVersion < version {
+			success = true
+		} else {
+			lockWrapper.ck.Put(lockWrapper.key, "", version)
+		}
+	}
+}
+
+func (lockWrapper *Lock) ReadUntilSuccess() (string, rpc.Tversion) {
+	success := false
+	var value string
+	var version rpc.Tversion
+	var err rpc.Err
+
+	for success == false {
+		//fmt.Println("stuck at ReadUntilSuccess")
+		value, version, err = lockWrapper.ck.Get(lockWrapper.key)
+		if err != rpc.OK {
+			continue
+		} else {
+			success = true
+		}
+	}
+
+	return value, version
 }
